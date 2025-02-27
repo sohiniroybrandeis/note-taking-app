84113352a0cd43d0b466a28f60d346418eb8850d
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-02-26 21:43:37 -0500

    Add files via upload

--- stream.py
+++ stream.py
@@ -1,16 +1,12 @@
 import streamlit as st
 from notes import Note, NoteBook
 
+# initialize session states
+
 if "notebook" not in st.session_state:
     st.session_state.notebook = NoteBook()
-nb = st.session_state.notebook  # Use the session-stored notebook
 
-
-# initialize session states
-if "notes" not in st.session_state:
-    st.session_state.notes = []
-if "titles" not in st.session_state:
-    st.session_state.titles = []
+nb = st.session_state.notebook
 
 st.title("NoteBook")
 st.markdown("Feel free to add and search for notes!")
@@ -24,7 +20,7 @@ if st.button("Search"):
 
         if results:
             for title in results:
-                st.write(f"- **{title}**")  # Displays matched titles
+                st.write(f"- **{title}**")
         else:
             st.warning("No matching notes found.")
 
@@ -35,18 +31,16 @@ content = st.text_area("Content")
 if st.button("Add"):
     if title:
         nb.add(title, content)
-        st.session_state.notes = nb.notes
-        st.session_state.titles.append(title)
         st.rerun()  # refresh
 
 # side panel to store notes
 with st.sidebar:
     st.write("### Notes")
-    if nb.notes:  # Check if there are notes
+    if nb.notes:
         selected_title = st.selectbox('Select a note:', [note.title for note in nb.notes])
 
         if selected_title:
-            note_obj = nb.retrieve_note(selected_title)  # Get the note object
-            if note_obj:
-                st.write(f"**Title:** {note_obj.title}")
-                st.write(f"**Content:** {note_obj.content}")
+            nt = nb.retrieve_note(selected_title)
+            if nt:
+                st.write(f"**Title:** {nt.title}")
+                st.write(f"**Content:** {nt.content}")
